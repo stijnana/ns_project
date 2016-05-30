1226a1b1d79c40bfc9eb77ab1e165d2444b8419b
Robinwist <robinwist@hotmail.com>
2016-05-30 09:20:48 +0200

    -

--- ns/app/controllers/students.controller.js
+++ ns/app/controllers/students.controller.js
@@ -1,35 +1,10 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('controllers')
-        .controller('StudentsController', StudentsController);
-
-    function StudentsController() {
-
-        var vm = this;
-
-        vm.studentName = "Gianni";
-
-        vm.setCurrentStudent = function(currentStudent){
-            if(currentStudent != vm.currentStudent)
-                vm.currentStudent = currentStudent;
-            else
-                vm.currentStudent = undefined;
+angular
+    .module('app.services', [])
+    .factory('Friend', function ($http) {
+        return {
+            get: function () {
+                console.log("inside function");
+                return $http.get('/api/get.json');
+            }
         };
-
-        vm.allStudents = [
-            {name:"Artur", age:20},
-            {name:"Mitchell", age:18},
-            {name:"Remco", age:17},
-            {name:"Dave", age:21},
-            {name:"Sander", age:17},
-            {name:"Jimmy", age:18},
-            {name:"Yassine", age:19}
-        ];
-
-
-    }
-
-
-})();
+    });
